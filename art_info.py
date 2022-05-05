21dd5d461d34fa3ccb7270f5df09c97750e293c4
Rishin Sharma <70151858+rishinsharma@users.noreply.github.com>
2022-05-05 16:18:20 -0400

    Delete art_info.py

--- art_info.py
+++
@@ -1,5 +0,0 @@
-# TODO Fill me out
-
-FILE_NAME = "NFT.jpg"
-ASSET_NAME = "COLTRANE_FIFTHS"
-ASSET_DESCRIPTION = "Depiction of the intersection of music and math. A visual representation of the relationships that exist within a chord"
