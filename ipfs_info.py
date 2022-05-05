81b8fbb770ca13f09f85fbe68d913ac7f31fe674
Rishin Sharma <70151858+rishinsharma@users.noreply.github.com>
2022-05-05 16:19:04 -0400

    Delete ipfs_info.py

--- ipfs_info.py
+++
@@ -1,6 +0,0 @@
-# TODO Fill me out
-
-ASSET_NAME = "COLTRANE_FIFTHS"
-IPFS_METADATA_ADDRESS = "ipfs://QmdS5KjvgPpmS1MxEsXsn86qcSG5PD8Np3rhrxyZN6ki76"
-# NOTE: the metadata hash should be a binary string
-IPFS_METADATA_HASH = b'Hx8qZ\xa6\xc1"\x88\xd8\xb7Lz7\x1b\xdft\x01\x19i\xc5\x10\xa1WS\xa4\x8e\xe9o\xb9\xf4\xff'
